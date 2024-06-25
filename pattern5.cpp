0b21c2faba79dca3ae297cef1026fbcf2119eaf1
Somnath Sengupta <136365858+SomnathSengupta@users.noreply.github.com>
2024-06-26 01:45:12 +0530

    Update pattern5.cpp

--- pattern5.cpp
+++ pattern5.cpp
@@ -4,33 +4,35 @@ int main()
 {
     //User input
     int n;
-    cin >>n;
+    cout << "Enter the no of rows: ";
+    cin >> n;
     //Outer loop for row
-    for (int i=1;i<=n;i++) {
+    for (int i = 1;i <= n; i++) {
         // Loop for space 
-        for (int s=1;s<=n-i;s++){
-            cout<<" ";
+        for (int s = 1;s <= n-i; s++){
+            cout << " ";
         }
         // Inner Loop for column
-        for (int j=1;j<=2*i-1;j++){
+        for (int j = 1; j <= 2*i - 1; j++){
             // Printing *
-            cout<<"*";
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
-    for (int i=n-1;i>=1;i--) {
-        for (int s=1;s<=n-i;s++){
-            cout<<" ";
+    for (int i = n - 1; i >= 1; i--) {
+        for (int s = 1; s <= n-i; s++) {
+            cout << " ";
         }
 
-        for (int j=1;j<=2*i-1;j++){
-            cout<<"*";
+        for (int j = 1; j <= 2*i - 1; j++){
+            cout << "*";
         }
         // Printing the next line 
-        cout<<endl;
+        cout << endl;
     }
 }
-/*OUTPUT 
+/*
+   Enter the no of rows: 4
    *
   ***
  *****
