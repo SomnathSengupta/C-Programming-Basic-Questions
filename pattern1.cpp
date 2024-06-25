cb391f261be4f901dcac00d6e2be53f971d777da
Somnath Sengupta <136365858+SomnathSengupta@users.noreply.github.com>
2024-06-25 10:03:51 +0530

    Update pattern1.cpp

--- pattern1.cpp
+++ pattern1.cpp
@@ -4,6 +4,7 @@ int main()
 {
 //user input 
     int n;
+    cout<<"Enter the no of rows: ";
     cin>>n;
 
 //Outer loop is for row 
@@ -20,3 +21,10 @@ for(int i=1;i<=n;i++)
     cout<<endl;
 }
 }
+/*
+Enter the no of rows: 4
+*
+**
+***
+****
+*/
