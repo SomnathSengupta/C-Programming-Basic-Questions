47422e47b81d232ffddaab4eb5bdcbfbdf588ef1
Rohit Chatterjee <rohitchatterjee758@gmail.com>
2024-07-12 16:01:29 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,22 @@
+num=int(input("Enter your number ")) 
+if num>=90 and num<=100: #90 - 100
+    print("Your Grade is :O") 
+elif num>=80 and num<=89: #80 - 89
+    print("Your Grade is :E") 
+elif num>=70 and num<=79: #70 - 79
+    print("Your Grade is :A") 
+elif num>=60 and num<=69: #60 - 69
+    print("Your Grade is :B") 
+ 
+elif num>=50 and num<=59: #50 - 59
+    print("Your Grade is :C") 
+elif num>=40 and num<=49: #40 - 49
+    print("Your Grade is :D") 
+elif num<=40 and num>=0: #0 - 39
+    print("Your Grade is :F") 
+else: 
+    print("Invalid")
+    
+    
+    #  Enter your number 40
+    #Your Grade is :D
